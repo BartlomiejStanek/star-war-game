0eb03c08c38fd58f8ad23dca5eb66141880d1506
bstanek <bstanek@future-processing.com>
2024-05-31 08:34:41 +0200

    change script name

--- src/app/containers/arena/arena.container.ts
+++ src/app/containers/arena/arena.container.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
@@ -6,7 +6,7 @@ import {Resource} from "@app/model/enum/resource";
 import {PeopleService} from "@app/api/services/people.service";
 import {StarshipService} from "@app/api/services/starship.service";
 import {Player} from "@app/model/calss/player";
-import {forkJoin} from "rxjs";
+import {forkJoin, Subscription} from "rxjs";
 import { CardComponent } from '@app/componets/card/card.component';
 import { CommonModule } from '@angular/common';
 
@@ -17,7 +17,8 @@ import { CommonModule } from '@angular/common';
   templateUrl: './arena.container.html',
   styleUrl: './arena.container.scss'
 })
-export class ArenaContainer {
+export class ArenaContainer implements OnDestroy{
+  private _subscription: Subscription[] = [];
   public readonly resourcesForm: FormControl<Resource | null> = new FormControl<Resource>(Resource.people);
   public readonly resources: Resource[] = [Resource.people, Resource.starships];
   public readonly player1: Player = new Player('Player 1');
@@ -40,23 +41,27 @@ export class ArenaContainer {
     this.isLoading = true;
     this.resourcesForm.disable();
     if(this.resourcesForm.value === Resource.people){
-      forkJoin([this._peopleService.getRandomPerson(),this._peopleService.getRandomPerson()]).subscribe(([person1, person2]) => {
+      this._subscription.push(forkJoin([this._peopleService.getRandomPerson(),this._peopleService.getRandomPerson()]).subscribe(([person1, person2]) => {
         this.player1.setResource(person1);
         this.player2.setResource(person2);
         this.isLoading = false;
         this.resourcesForm.enable();
-      });
+      }));
     } else {
-      forkJoin([this._starshipService.getRandomStarship(), this._starshipService.getRandomStarship()]).subscribe(([starship1, starship2]) => {
+      this._subscription.push(forkJoin([this._starshipService.getRandomStarship(), this._starshipService.getRandomStarship()]).subscribe(([starship1, starship2]) => {
         this.player1.setResource(starship1);
         this.player2.setResource(starship2);
         this.isLoading = false;
         this.resourcesForm.enable();
-      });
+      }));
     }
   }
 
   public play(): void {
     this.winner = Player.getWinner(this.player1, this.player2);
+  }
+
+  public ngOnDestroy(): void {
+    this._subscription.forEach(subscription => subscription.unsubscribe());
   }
 }
